ad60498124a53a69903a5ba6abeaa674ba9bef45
Jasmine White <jasmine@whitepdx.com>
2024-02-28 23:15:52 -0700

    created js file

---
+++ script.js
@@ -0,0 +1,6 @@
+// Import Statement for Prompt-Sync Library 
+// const prompt = require('prompt-sync')({sigint: true});
+
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+console.log("Welcome to the Laundromat!");
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~");
