b1f8e6070143e066d73f6392e6cd474b5c03dd93
Jasmine White <jasmine@whitepdx.com>
2024-03-02 19:39:14 -0700

    ASCII art

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Import Statement for Prompt-Sync Library 
 // const prompt = require('prompt-sync')({sigint: true});
 
-console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~");
-console.log("Welcome to the Laundromat!");
-console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+console.log("#     #   # # # #   #         #         # # #   #       #     #   ");
+console.log("#     #   #         #         #        #     #  #       #     #   ");
+console.log("# # # #   # # # #   #         #        #     #  #     #         # ");
+console.log("#     #   #         #         #        #     #         #       #  ");
+console.log("#     #   # # # #   # # # #   # # # #   # # #   #        # # #    ");
+
+
+
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+console.log("              Welcome to CleanCode Laundromat!           ");
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
