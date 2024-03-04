c65bc8b15976187706f3bcdb86fedeeea44ecfd7
Jasmine White <jasmine@whitepdx.com>
2024-03-03 20:46:23 -0700

    finished menu

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 console.log("     (╯^ - ^)╯  Welcome to CleanCode Laundromat!  ノ(^ - ^ノ)      ");
 console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
+
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+console.log("|                Menu                    |");
+console.log("------------------------------------------");
+console.log("| Services:                              |");
+console.log("| - 🌈 Color Load:        $2.50 per pound |");
+console.log("| - ⚪ Whites Load:       $4.00 per pound |");
+console.log("| - ⚫ Darks Load:        $6.00 per pound |");
+console.log("|----------------------------------------|");
+console.log("| Additional Services:                   |");
+console.log("| - 🧼 Detergent:         $1.50 per load  |");
+console.log("| - 🧸 Softener:          $1.00 per load  |");
+console.log("| - 🧺 Dryer Sheets:      $0.50 each      |");
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
