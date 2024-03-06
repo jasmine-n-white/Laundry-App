4dde4667956d86ca8a496e555a9ee3effe86c0a3
Jasmine White <jasmine@whitepdx.com>
2024-03-05 17:34:28 -0700

    testing load and price calculation

--- script.js
+++ script.js
@@ -26,9 +26,40 @@ console.log("| - 🧸 Softener:          $1.00 per load  |");
 console.log("| - 🧺 Dryer Sheets:      $0.50 each      |");
 console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
+let loads = 0;
+
 let colorPounds = prompt("How many pounds of color clothes would you like to wash?");
+if (colorPounds > 20) {
+    loads += Math.ceil(colorPounds/10);
+}
+if (colorPounds > 10 && colorPounds <= 20) {
+    loads +=2;
+}
+if (colorPounds <= 10) {
+    loads += 1;
+}
+
 let whitePounds = prompt("How many pounds of white clothes would you like to wash?");
+if (whitePounds > 20) {
+    loads += Math.ceil(whitePounds/10);
+}
+if (whitePounds > 10 && whitePounds <= 20) {
+    loads +=2;
+}
+if (whitePounds <= 10) {
+    loads += 1;
+}
+
 let darkPounds = prompt("How many pounds of dark clothes would you like to wash?");
+if (darkPounds > 20) {
+    loads += Math.ceil(darkPounds/10);
+}
+if (darkPounds > 10 && darkPounds <= 20) {
+    loads +=2;
+}
+if (darkPounds <= 10) {
+    loads += 1;
+}
 
 let totalPounds = colorPounds + whitePounds + darkPounds;
 
@@ -39,20 +70,25 @@ let darkPrice = Number(darkPounds) * 6;
 
 let detergentPrice;
 let softenerPrice;
-let dryerSheetPrice;
 
 let detergent = prompt("Do you need detergent (true/false)?");
-let softener = prompt("Do you need fabric softener (true/false)?");
-let dryerSheets = prompt("How many dryer sheets per drying load?");
-
-if (detergent == true) {
-    detergentPrice = 1.5;
+if (detergent == "true") {
+    detergentPrice = 1.5 * loads;
 } else {
     detergentPrice = 0;
 }
-
-if (softener == true) {
-    softenerPrice = 1;
+let softener = prompt("Do you need fabric softener (true/false)?");
+if (softener == "true") {
+    softenerPrice = 1 * loads;
 } else {
     softenerPrice = 0;
 }
+let dryerSheets = prompt("How many dryer sheets per drying load?");
+let dryerSheetPrice = Number(dryerSheets) * loads * 0.5;
+dryerSheetPrice = parseFloat(dryerSheetPrice);
+
+
+console.log(dryerSheetPrice);
+console.log(softenerPrice);
+console.log(detergentPrice);
+console.log(loads);
